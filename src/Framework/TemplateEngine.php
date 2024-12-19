b4dab0522731c409b43cc8f560ed5c333706d4be
Sebastian <sebastian.jast.programista@gmail.com>
2024-12-19 20:20:59 +0100

    Rendering a Template

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -7,4 +7,8 @@ namespace Framework;
 class TemplateEngine
 {
     public function __construct(private string $basePath) {}
+
+    public function render(string $template) {
+        include "{$this->basePath}/{$template}";
+    }
 }
