ba920e665a010e3cdf13a251f15f7cc471df997a
Sebastian <sebastian.jast.programista@gmail.com>
2024-12-19 21:11:39 +0100

    Creating an Output Buffer

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -18,6 +18,6 @@ class HomeController
 
     public function home()
     {
-        $this->view->render("/index.php", ['title' => 'Home page']);
+        echo $this->view->render("/index.php", ['title' => 'Home page']);
     }
 }
