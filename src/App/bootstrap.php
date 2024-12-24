98755b91c7a20062da678349737bdfe38a286dad
Sebastian <sebastian.jast.programista@gmail.com>
2024-12-24 16:03:40 +0100

    Adding Middleware

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,10 +6,11 @@ include __DIR__ . "/../../vendor/autoload.php";
 
 use App\Config\Paths;
 use Framework\App;
-use function App\Config\registerRoutes;
+use function App\Config\{registerRoutes, registerMiddleware};
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
 registerRoutes($app);
+registerMiddleware($app);
 
 return $app;
