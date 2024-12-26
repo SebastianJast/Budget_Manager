d300b3c63fee973120cffd54c8a80c4f2868b199
“SebastianJast” <sebastian.jast.programista@gmail.com>
2024-12-26 20:01:17 +0100

    Update title

--- src/App/views/partials/_header.php
+++ src/App/views/partials/_header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Budget Manager</title>
+    <title><?php echo e($title); ?> - Budget Manager</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
