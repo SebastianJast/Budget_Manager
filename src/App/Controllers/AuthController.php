f229dfcdb2c3f007d5e600cafefc7aed38600902
“SebastianJast” <sebastian.jast.programista@gmail.com>
2024-12-27 13:39:32 +0100

    Add ValidatorService

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\ValidatorService;
 use Framework\TemplateEngine;
 
 class AuthController
 {
-    public function __construct(private TemplateEngine $view) {}
+    public function __construct(private TemplateEngine $view, private ValidatorService $validatorService) {}
 
     public function registerView()
     {
