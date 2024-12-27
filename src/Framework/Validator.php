4cf30f581b2f870b0ba873c313c18aae80413f6c
“SebastianJast” <sebastian.jast.programista@gmail.com>
2024-12-27 23:55:36 +0100

    Passing on the Error

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -32,7 +32,7 @@ class Validator
             }
         }
         if (count($errors)) {
-            throw new ValidationException();
+            throw new ValidationException($errors);
         }
     }
 }
