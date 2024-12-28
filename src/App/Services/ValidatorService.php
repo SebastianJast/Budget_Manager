45e5f4e7707fb1e85e1d387c182b36d7360626b1
“SebastianJast” <sebastian.jast.programista@gmail.com>
2024-12-28 14:30:24 +0100

    Validating Emails

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Framework\Rules\RequiredRule;
+use Framework\Rules\{RequiredRule, EmailRule};
 use Framework\Validator;
 
 class ValidatorService
@@ -16,13 +16,14 @@ class ValidatorService
         $this->validator = new Validator();
 
         $this->validator->add('required', new RequiredRule());
+        $this->validator->add('email', new EmailRule());
     }
 
     public function validateRegister(array $formData)
     {
         $this->validator->validate($formData, [
             'login' => ['required'],
-            'email' => ['required'],
+            'email' => ['required', 'email'],
             'password' => ['required'],
             'confirmPassword' => ['required'],
             'terms' => ['required']
