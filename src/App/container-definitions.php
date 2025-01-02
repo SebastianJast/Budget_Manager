d6e9a70b61b6a412e24ba7bba23c57e82ec2383e
“SebastianJast” <sebastian.jast.programista@gmail.com>
2025-01-02 21:32:10 +0100

    Database Container Definition

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 use Framework\TemplateEngine;
 use App\Config\Paths;
 use App\Services\ValidatorService;
+use Framework\Database;
 
 return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn() => new ValidatorService(),
+    Database::class => fn() => new Database(
+        'mysql',
+        [
+            'host' => '127.0.0.1',
+            'port' => 3306,
+            'dbname' => 'budget_manager',
+        ],
+        'root',
+        ''
+    )
 ];
