734931264caf9816df07e92c0e393c443ef2d2fd
“SebastianJast” <sebastian.jast.programista@gmail.com>
2025-01-03 13:41:24 +0100

    Passing on the Container to Definitions

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 use Framework\TemplateEngine;
 
 class AuthController
 {
-    public function __construct(private TemplateEngine $view, private ValidatorService $validatorService) {}
+    public function __construct(private TemplateEngine $view, private ValidatorService $validatorService, private UserService $userService) {}
 
     public function registerView()
     {
