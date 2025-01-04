85129d584320a24063c541aaba881f35ce540ad8
“SebastianJast” <sebastian.jast.programista@gmail.com>
2025-01-04 14:28:47 +0100

    Validating CSRF Tokens

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{FlashMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware, SessionMiddleware, CsrfTokenMiddleware};
+use App\Middleware\{FlashMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware, SessionMiddleware, CsrfTokenMiddleware, CsrfGuardMiddleware};
 
 function registerMiddleware(App $app)
 {
+    $app->addMiddleware(CsrfGuardMiddleware::class);
     $app->addMiddleware(CsrfTokenMiddleware::class);
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
