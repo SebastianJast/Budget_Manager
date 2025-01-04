1227c0b37d4e03ed6032bac1e2fe1c7f5c14e271
“SebastianJast” <sebastian.jast.programista@gmail.com>
2025-01-04 12:54:52 +0100

    Applying Route Middleware

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -6,13 +6,15 @@ namespace App\Config;
 
 use Framework\App;
 use App\Controllers\{AuthController, HomeController, MainController};
+use App\Middleware\AuthRequiredMiddleware;
+use App\Middleware\GuestOnlyMiddleware;
 
 function registerRoutes(App $app)
 {
-    $app->get('/', [HomeController::class, 'home']);
-    $app->get('/main', [MainController::class, 'main']);
-    $app->get('/register', [AuthController::class, 'registerView']);
-    $app->post('/register', [AuthController::class, 'register']);
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->post('/login', [AuthController::class, 'login']);
+    $app->get('/', [HomeController::class, 'home'])->add(AuthRequiredMiddleware::class);
+    $app->get('/main', [MainController::class, 'main'])->add(GuestOnlyMiddleware::class);
+    $app->get('/register', [AuthController::class, 'registerView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
+    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
 }
