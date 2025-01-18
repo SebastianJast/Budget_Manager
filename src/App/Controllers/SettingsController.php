dfba47418d1e8f4b29f60b114b9ca5dd9cd7570e
“SebastianJast” <sebastian.jast.programista@gmail.com>
2025-01-18 15:55:46 +0100

    Add income categories to the form as radio buttons

--- src/App/Controllers/SettingsController.php
+++ src/App/Controllers/SettingsController.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\IncomeService;
 use Framework\TemplateEngine;
 
 class SettingsController
 {
-    public function __construct(private TemplateEngine $view) {}
+    public function __construct(private TemplateEngine $view, private IncomeService $incomeService) {}
 
     public function createView()
     {
-        echo $this->view->render("/settings.php");
+        $incomesCategories = $this->incomeService->selectCategory();
+
+        echo $this->view->render("/settings.php", ['incomesCategories' => $incomesCategories]);
     }
 }
--- src/App/views/settings.php
+++ src/App/views/settings.php
@@ -1,2 +1,104 @@
 <?php include $this->resolve("partials/_header.php") ?>
+<main>
+    <h2 class="display-5 fw-bold text-white lh-1 mt-4 text-center mb-4">
+        Dostępne opcje edycji
+    </h2>
+    <div
+        class="row d-flex flex-column flex-lg-row justify-content-center align-items-center gap-4 row-cols-1 row-cols-md-2 mb-3 text-center">
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <p class="my-0 fw-normal">Edycja nazwy kategorii</p>
+                </div>
+                <div class="card-body d-flex">
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#editIncomeCategoryForm"
+                        href="#">Przychody</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Wydatki</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Sposoby płatności</a>
+                </div>
+                <div class="card-header py-3">
+                    <p class="my-0 fw-normal">Dodanie nowej kategorii</p>
+                </div>
+                <div class="card-body d-flex">
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Przychody</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Wydatki</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Sposoby płatności</a>
+                </div>
+                <div class="card-header py-3">
+                    <p class="my-0 fw-normal">Usuwanie kategorii</p>
+                </div>
+                <div class="card-body d-flex">
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Przychody</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Wydatki</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Sposoby płatności</a>
+                </div>
+                <div class="card-header py-3">
+                    <p class="my-0 fw-normal">Edycja konta Użytkownika</p>
+                </div>
+                <div class="card-body">
+                    <a class="dropdown-item text-decoration-none text-dark active my-3" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Edycja danych Użytkownika</a>
+                    <a class="dropdown-item text-decoration-none text-dark active" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="#">Usuń swoje konto</a>
+                </div>
+                <div class="card-header py-3">
+                    <p class="my-0 fw-normal">Powrót do menu głównego</p>
+                </div>
+            </div>
+            <div
+                class="modal fade"
+                id="editIncomeCategoryForm"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header position-relative" style="height: 100px;">
+                            <h5 class="modal-title position-absolute start-50 top-50 translate-middle" id="exampleModalLabel">
+                                Wybierz kategorię do edycji i podaj nową nazwę:
+                            </h5>
+                            <button
+                                type="button"
+                                class="btn-close position-absolute top-0 end-0 me-2 mt-2"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <form method="GET">
+                            <div class="modal-body">
+                                <div class="form-floating my-1">
+                                    <input type="text" class="form-control" id="textInput" name="" required />
+                                    <label for="textInput">Nazwa nowej kategorii</label>
+                                </div>
+                            </div>
+                            <div class="w-50 mx-auto">
+                                <?php foreach ($incomesCategories as $incomeCategory): ?>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="<?php echo $incomeCategory['name']; ?>" checked>
+                                        <label class="form-check-label" for="exampleRadios1">
+                                            <?php echo $incomeCategory['name']; ?>
+                                        </label>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                    Close
+                                </button>
+                                <button type="submit" class="btn btn-primary" name="submitDate">
+                                    Ok
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+</main>
 <?php include $this->resolve("partials/_footer.php") ?>
